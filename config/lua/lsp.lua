5665901fedab549944ec9247531987ee77151894
Marvin Mann <marvin1995mann@gmail.com>
2024-03-03 22:11:23 +0100

    enable telescope and a lot of tweaks

--- config/lua/lsp.lua
+++ config/lua/lsp.lua
@@ -1,6 +1,12 @@
+local util = require "lspconfig/util"
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
@@ -9,48 +15,141 @@ vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<C
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'pylsp', 'cmake', 'yamlls', 'elmls', 'html', 'lua_ls', 'rnix', 'sqls', 'rust_analyzer', 'bashls' };
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- capabilities make the ls aware of the options of the editor?
+--
+-- could use a loop here -> most likely nothing is done here anyways
+-- rope etc are packaged by python311 anyways
+require('lspconfig').pylsp.setup({
     on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
-end
+    flags = {},
+    capabilities = capabilities,
+    plugins = {
+        rope = {
+            enabled = true,
+        },
+        pyflakes = {
+            enabled = true,
+        },
+        mccabe = {
+            enabled = false,
+        },
+        pycodestyle = {
+            enabled = true,
+        },
+        pydocstyle = {
+            enabled = true,
+        },
+        autopep8 = {
+            enabled = true,
+        },
+        yapf = {
+            enabled = true,
+        },
+        flake8 = {
+            enabled = true,
+        },
+        pylint = {
+            enabled = true,
+        },
+    },
+})
 
--- disable "vim not defined" warning inside lua
-require'lspconfig'.lua_ls.setup {
-    -- ... other configs
+require('lspconfig').gopls.setup({
+    on_attach = on_attach,
+    flags = {},
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            completeUnimported = true,
+        },
+    },
+    capabilities = capabilities,
+})
+
+require('lspconfig').cmake.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').yamlls.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').elmls.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').html.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').lua_ls.setup({
+    on_attach = on_attach,
+    flags = {},
     settings = {
         Lua = {
             diagnostics = {
                 globals = { 'vim' }
             }
         }
-    }
-}
+    },
+    capabilities = capabilities,
+})
 
+require('lspconfig').rnix.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').sqls.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
+
+require('lspconfig').rust_analyzer.setup({
+    on_attach = on_attach,
+    flags = {},
+    settings = {
+            ['rust-analyzer'] = {},
+    },
+    capabilities = capabilities,
+})
+
+require('lspconfig').bashls.setup({
+    on_attach = on_attach,
+    flags = {},
+    capabilities = capabilities,
+})
--- config/lua/telescope.lua
+++ config/lua/telescope.lua
@@ -1,6 +1,40 @@
-local opt = { noremap = true }
 local telescope = require("telescope")
-telescope.setup({})
+telescope.setup({
+    defaults = require("telescope.themes").get_ivy {
+    },
+    terminal_colors = true,
+})
+
+-- pcall(require('telescope').load_extension, 'fzf')
+-- pcall(require('telescope').load_extension, 'ui-select')
+
+
+local opt = { noremap = true }
 telescope.load_extension("recent_files")
-vim.api.nvim_set_keymap("n", "<leader><tab>", ":lua require('telescope.builtin').find_files()<CR>", opt)
-vim.api.nvim_set_keymap("n", "<leader><leader>", ":lua require('telescope').extensions.recent_files.pick()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader><tab>", ":lua require('telescope.builtin').find_files()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader><leader>", ":lua require('telescope').extensions.recent_files.pick()<CR>", opt)
+vim.api.nvim_set_keymap("", "leader>sh", ":lua require('telescope.builtin').help_tags()<CR>", opt)
+vim.api.nvim_set_keymap("", "leader>sk", ":lua require('telescope.builtin').keymaps()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>sf",":lua require('telescope.builtin').find_files()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>ss",":lua require('telescope.builtin').builtin()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>sw",":lua require('telescope.builtin').grep_string()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>sg",":lua require('telescope.builtin').live_grep()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>sd",":lua require('telescope.builtin').diagnostics()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>sr",":lua require('telescope.builtin').resume()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader>s.",":lua require('telescope.builtin').oldfiles()<CR>", opt)
+
+-- See all class names 
+-- https://github.com/nvim-telescope/telescope.nvim/blob/aa83606299c5beeaf80e656efbf07bde258db7be/plugin/telescope.lua#L28
+-- gruvbox color plate
+-- https://github.com/gruvbox-community/gruvbox
+vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeSlectionCaret", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeMultiSelection", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopePrompt", { fg = "#111111" })
