f9affa26fbbea49324a2f735cd30d60bc1a9af3b
Marvin Mann <marvin1995mann@gmail.com>
2024-03-28 00:33:00 +0100

    prepared telescope keybindings for reconfiguration

--- config/lua/telescope.lua
+++ config/lua/telescope.lua
@@ -7,30 +7,26 @@ telescope.setup({
 
 local opt = { noremap = true }
 telescope.load_extension("recent_files")
-vim.api.nvim_set_keymap("", "<leader><tab>", ":lua require('telescope.builtin').find_files()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader><leader>", ":lua require('telescope').extensions.recent_files.pick()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sh", ":lua require('telescope.builtin').help_tags()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sk", ":lua require('telescope.builtin').keymaps()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sf", ":lua require('telescope.builtin').find_files()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>ss", ":lua require('telescope.builtin').builtin()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sw", ":lua require('telescope.builtin').grep_string()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sg", ":lua require('telescope.builtin').live_grep()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sd", ":lua require('telescope.builtin').diagnostics()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>sr", ":lua require('telescope.builtin').resume()<CR>", opt)
-vim.api.nvim_set_keymap("", "<leader>s.", ":lua require('telescope.builtin').oldfiles()<CR>", opt)
+vim.api.nvim_set_keymap("", "<leader><leader>", ":lua require('telescope').extensions.recent_files.pick()<CR>", opt) -- lists recently opened files
+vim.api.nvim_set_keymap("", "<leader>sf", ":lua require('telescope.builtin').find_files()<CR>", opt)                 -- search for files respect gitignore
+vim.api.nvim_set_keymap("", "<leader>ss", ":lua require('telescope.builtin').builtin()<CR>", opt)                    -- lists Built-in pickers
+vim.api.nvim_set_keymap("", "<leader>sw", ":lua require('telescope.builtin').grep_string()<CR>", opt)                -- searches for the string under your cursor or the visual selection in your current working directory
+vim.api.nvim_set_keymap("", "<leader>sg", ":lua require('telescope.builtin').live_grep()<CR>", opt)                  -- search for a string
+vim.api.nvim_set_keymap("", "<leader>sd", ":lua require('telescope.builtin').diagnostics()<CR>", opt)                -- lists diagnostics for all open buffers
+vim.api.nvim_set_keymap("", "<leader>sr", ":lua require('telescope.builtin').resume()<CR>", opt)                     -- lists the results incl. multi-selections of the previous picker
+vim.api.nvim_set_keymap("", "<leader>sh", ":lua require('telescope.builtin').help_tags()<CR>", opt)                  -- lists nvim help tags
+vim.api.nvim_set_keymap("", "<leader>sk", ":lua require('telescope.builtin').keymaps()<CR>", opt)                    -- lists keymaps
 
 -- See all class names
 -- https://github.com/nvim-telescope/telescope.nvim/blob/aa83606299c5beeaf80e656efbf07bde258db7be/plugin/telescope.lua#L28
--- gruvbox color plate
--- https://github.com/gruvbox-community/gruvbox
-vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeSlectionCaret", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeMultiSelection", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#111111" })
-vim.api.nvim_set_hl(0, "TelescopePrompt", { fg = "#111111" })
+vim.api.nvim_set_hl(0, "TelescopeSelection", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeSlectionCaret", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeMultiSelection", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopeMatching", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { ctermfg = "white" })
+vim.api.nvim_set_hl(0, "TelescopePrompt", { ctermfg = "white" })
