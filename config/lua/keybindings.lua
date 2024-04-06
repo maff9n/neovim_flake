7635beb923d65fc169ee8f95899663d3fb531003
Marvin Mann <marvin1995mann@gmail.com>
2024-04-06 22:05:53 +0200

    restored shortcut for line swap

--- config/lua/keybindings.lua
+++ config/lua/keybindings.lua
@@ -38,10 +38,8 @@ vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('n', '<', '<<')
 vim.keymap.set('n', '>', '>>')
 
-vim.keymap.set('n', 'J', '>+1<CR>gv=gv')
-vim.keymap.set('n', 'K', '<-2<CR>gv=gv')
-vim.keymap.set('n', 'J', '.+1<CR>==')
-vim.keymap.set('n', 'K', '.-2<CR>==')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
