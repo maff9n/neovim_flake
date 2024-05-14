d6c8c571e0f16736f506eb09fb927caca8fa35ea
Marvin Mann <marvin1995mann@gmail.com>
2024-05-14 23:11:04 +0200

    enabled cmp completion in command line

--- lua/cmp.lua
+++ lua/cmp.lua
@@ -1,16 +1,11 @@
--- Setup nvim-cmp.
-
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 local cmp = require 'cmp'
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
     snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            -- vim.fn["luasnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body)
         end,
     },
     window = {
@@ -18,8 +13,8 @@ cmp.setup({
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = {
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+            ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+            ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
             ['<C-y>'] = cmp.mapping.confirm({ select = true }),
             ["<C-Space>"] = cmp.mapping.complete(),
             ['<Tab>'] = nil,
@@ -32,3 +27,27 @@ cmp.setup({
         { name = 'buffer' },
     })
 })
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline(':', {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = cmp.config.sources({
+--         { name = 'path' },
+--         { name = 'buffer' }
+--     }, {
+--         {
+--             name = 'cmdline',
+--             option = {
+--                 ignore_cmds = { 'Man', '!' }
+--             }
+--         }
+--     })
+-- })
